409bf45d682650bb228cca07f8039467d4c36694
suishanwen <suishanwen@icloud.com>
2019-05-31 10:34:35 +0800

    U state

--- src/main/java/com/sw/note/model/CtrlClient.java
+++ src/main/java/com/sw/note/model/CtrlClient.java
@@ -9,93 +9,14 @@ public class CtrlClient {
     private Integer startNum;
     private Integer endNum;
     private String workerId;
+    private Integer workerInput;
     private Integer tail;
     private Integer autoVote;
+    private Integer overAuto;
     private String user;
     private Integer sort;
     private Integer state;
 
     public CtrlClient() {
-    }
-
-    public String getIdentity() {
-        return identity;
-    }
-
-    public void setIdentity(String identity) {
-        this.identity = identity;
-    }
-
-    public String getUname() {
-        return uname;
-    }
-
-    public void setUname(String uname) {
-        this.uname = uname;
-    }
-
-    public Integer getStartNum() {
-        return startNum;
-    }
-
-    public void setStartNum(Integer startNum) {
-        this.startNum = startNum;
-    }
-
-    public Integer getEndNum() {
-        return endNum;
-    }
-
-    public void setEndNum(Integer endNum) {
-        this.endNum = endNum;
-    }
-
-    public String getWorkerId() {
-        return workerId;
-    }
-
-    public void setWorkerId(String workerId) {
-        this.workerId = workerId;
-    }
-
-    public Integer getTail() {
-        return tail;
-    }
-
-    public void setTail(Integer tail) {
-        this.tail = tail;
-    }
-
-    public Integer getAutoVote() {
-        return autoVote;
-    }
-
-    public void setAutoVote(Integer autoVote) {
-        this.autoVote = autoVote;
-    }
-
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public Integer getSort() {
-        return sort;
-    }
-
-    public void setSort(Integer sort) {
-        this.sort = sort;
-    }
-
-    public Integer getState() {
-        return state;
-    }
-
-    public void setState(Integer state) {
-        this.state = state;
     }
 }
