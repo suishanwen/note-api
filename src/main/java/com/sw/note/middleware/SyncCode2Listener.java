77ff216f29d9bceab0614c18cce54e4e8c15d76b
suishanwen <suishanwen@icloud.com>
2019-06-03 11:16:42 +0800

    A queue

---
+++ src/main/java/com/sw/note/middleware/SyncCode2Listener.java
@@ -0,0 +1,26 @@
+package com.sw.note.middleware;
+
+import com.rabbitmq.client.Channel;
+import com.sw.note.service.VoteService;
+import com.sw.note.util.MqUtil;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnMessage;
+
+@Component
+@RabbitListener(queues = "sync2")
+public class SyncCode2Listener {
+
+    @Autowired
+    VoteService voteService;
+
+    @RabbitHandler
+    @OnMessage
+    public void receiver(Channel channel, Message message, String data) {
+        MqUtil.consume(channel, message, data, MqUtil.TYPE_MOBILE2);
+    }
+}
--- src/main/java/com/sw/note/util/MqUtil.java
+++ src/main/java/com/sw/note/util/MqUtil.java
@@ -15,6 +15,7 @@ public class MqUtil {
 
     public static final int TYPE_CLIENT = 1;
     public static final int TYPE_MOBILE = 2;
+    public static final int TYPE_MOBILE2 = 3;
 
     private static Logger logger = LoggerFactory.getLogger(MqUtil.class);
 
@@ -34,6 +35,8 @@ public class MqUtil {
             String identity = ctrlCode.getIdentity();
             if (type == TYPE_MOBILE) {
                 identity += "_mobi";
+            } else if (type == TYPE_MOBILE2) {
+                identity += "_mobi2";
             }
             if (CtrlDeliverSocket.wsClientMap.containsKey(identity)) {
                 CtrlDeliverSocket ctrlDeliverSocket = CtrlDeliverSocket.wsClientMap.get(identity);
