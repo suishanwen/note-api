bc6775cb82dbb6ad2df045f8c3c6eb841fa565cd
suishanwen <suishanwen@icloud.com>
2019-06-12 10:35:35 +0800

    A baiduApi - ocr

--- src/main/java/com/sw/note/api/FileController.java
+++ src/main/java/com/sw/note/api/FileController.java
@@ -3,6 +3,7 @@ package com.sw.note.api;
 import com.sw.note.service.FileService;
 import com.sw.note.util.DateUtil;
 import io.swagger.annotations.*;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +15,18 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.UUID;
 
+import com.baidu.aip.ocr.AipOcr;
+
+import java.util.HashMap;
 
 @Api(value = "文件", description = "文件", tags = "3")
 @RestController
 @RequestMapping(path = "/file")
 public class FileController {
+    //设置APPID/AK/SK
+    private static final String APP_ID = "16491720";
+    private static final String API_KEY = "NDxSBdoXvWBVGS0u0BfKYmNB";
+    private static final String SECRET_KEY = "osdLxGFfNkZ443hzRFMnBxIhUMVh6f2u";
 
     @Autowired
     FileService fileService;
@@ -51,5 +59,19 @@ public class FileController {
     @PostMapping(value = "trumpTwitter")
     public void trumpTwitter() {
         fileService.trumpTwitter();
+    }
+
+
+    @ApiOperation(value = "识别图片中的文字", notes = "识别图片中的文字")
+    @PostMapping(value = "imgocr", headers = "content-type=multipart/form-data")
+    public JSONObject orc(@ApiParam(value = "文件", required = true) @RequestBody MultipartFile file) throws IOException {
+        // 初始化一个AipImageClassify
+        AipOcr client = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
+        // 可选：设置网络连接参数
+        client.setConnectionTimeoutInMillis(2000);
+        client.setSocketTimeoutInMillis(60000);
+        // 调用接口
+        return client.basicGeneral(file.getBytes(), new HashMap<String, String>());
+
     }
 }
