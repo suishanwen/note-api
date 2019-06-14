476b42a9d871a41361c0b978bcd3371c5cb1362a
suishanwen <suishanwen@icloud.com>
2019-06-14 15:40:52 +0800

    A mail api

---
+++ src/main/java/com/sw/note/api/EmailController.java
@@ -0,0 +1,28 @@
+package com.sw.note.api;
+
+import com.sw.note.util.MailUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "邮件", description = "邮件", tags = "6")
+@RestController
+@RequestMapping(path = "/email")
+public class EmailController {
+    @ApiOperation(value = "发送", notes = "发送")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "receiver", paramType = "query", value = "receiver", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "receiver", paramType = "query", value = "receiver", required = true, dataType = "String")
+    })
+    @GetMapping("/send")
+    public void send(@RequestParam("receiver") String receiver, @RequestParam("subject") String subject, @RequestParam("content") String content) {
+        MailUtils cn = new MailUtils();
+        cn.setAddress("controlservice@sina.com", receiver, subject);
+        cn.send("smtp.sina.com", "controlservice@sina.com", "a123456", content);
+    }
+}
