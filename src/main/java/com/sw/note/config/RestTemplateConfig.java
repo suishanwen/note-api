9a0ef49b21d5fa496d8f2b10e477fa1a6771f817
suishanwen <suishanwen@icloud.com>
2019-09-29 11:49:11 +0800

    reset timeout

--- src/main/java/com/sw/note/config/RestTemplateConfig.java
+++ src/main/java/com/sw/note/config/RestTemplateConfig.java
@@ -16,8 +16,8 @@ public class RestTemplateConfig {
     @Bean
     public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(10000);
-        factory.setConnectTimeout(10000);
+        factory.setReadTimeout(100000);
+        factory.setConnectTimeout(100000);
         return factory;
     }
 
