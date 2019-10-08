a2c0bc5b7dc38345763e6acca4b10edf518edda6
suishanwen <suishanwen@icloud.com>
2019-10-08 18:30:26 +0800

    bug fix

--- src/main/java/com/sw/note/timer/ClientDirectTimerReset.java
+++ src/main/java/com/sw/note/timer/ClientDirectTimerReset.java
@@ -57,7 +57,7 @@ public class ClientDirectTimerReset {
             }
             running = false;
         };
-        ScheduledExecutorUtil.scheduleAtFixedRate(runnable, 0, 600);
+        ScheduledExecutorUtil.scheduleAtFixedRate(runnable, 0, 1800);
     }
 
     private MultiValueMap<String, String> generateData(String instance) {
