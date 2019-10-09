0bcf62bcd079b42011c9cb1b2db94cd00c202da8
suishanwen <suishanwen@icloud.com>
2019-10-09 11:08:34 +0800

    tx bugfix

--- src/main/java/com/sw/note/timer/VoteProjectTimerTx.java
+++ src/main/java/com/sw/note/timer/VoteProjectTimerTx.java
@@ -18,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -49,9 +50,10 @@ public class VoteProjectTimerTx {
                 List<VoteProject> voteProjectList = analyzeHtml(html);
                 saveVoteProject(voteProjectList);
             } catch (Exception e) {
-                if(!e.getMessage().contains("SocketTimeoutException")){
+                if (!e.getMessage().contains("SocketTimeoutException")) {
                     clientDirectService.bugReport("server-tx", e.getMessage());
-                }            }
+                }
+            }
             running = false;
         };
         ScheduledExecutorUtil.scheduleAtFixedRate(runnable, 0, 10);
@@ -67,11 +69,14 @@ public class VoteProjectTimerTx {
 
 
     private List<VoteProject> analyzeHtml(String html) {
+        Calendar cale = Calendar.getInstance();
+        int month = cale.get(Calendar.MONTH) + 1;
+        int day = cale.get(Calendar.DATE);
         List<VoteProject> voteProjectList = Lists.newArrayList();
         Elements tableElements = Jsoup.parse(html).select("table[style=\"border-left:1px solid #009cec; border-right:1px solid #009cec; background-color:#FFF\"]").select("tr");
         Date date = new Date();
-        for (int i = 1; i < tableElements.size(); i++) {
-            Elements tdElements = tableElements.get(i).select("td");
+        for (Element tableElement : tableElements) {
+            Elements tdElements = tableElement.select("td");
             Element backgroundTd = tdElements.get(12);
             String backGrounInfo = backgroundTd.text();
             if (backGrounInfo != null) {
@@ -95,8 +100,19 @@ public class VoteProjectTimerTx {
             if (!tdElements.get(3).text().contains("新任务") && !tdElements.get(3).text().contains("未知")) {
                 hot = Double.valueOf(tdElements.get(3).text().split("/")[0].substring(1).trim()).intValue();
             }
+            String quantityInfo = tdElements.get(7).attr("title").split(String.format("%d-%d", month, day))[0];
             long finishQuantity = 0;
             long totalRequire = 0;
+            if (quantityInfo.contains("/")) {
+                try {
+                    finishQuantity = Long.parseLong(quantityInfo.split("/")[0]);
+                } catch (Exception e) {
+                }
+                try {
+                    totalRequire = Long.parseLong(quantityInfo.split("/")[1]);
+                } catch (Exception e) {
+                }
+            }
             long remains = Long.parseLong(tdElements.get(7).text());
             VoteProject voteProject = new VoteProject(projectName, ipDial, hot, price, totalRequire, finishQuantity, remains, backGroundNo, backgroundAddress, downloadAddress, "TX", date);
             voteProjectList.add(voteProject);
