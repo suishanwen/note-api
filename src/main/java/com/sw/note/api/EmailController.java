65a32ed6918a44c37f4078befaf178d3b0853ed7
suishanwen <suishanwen@icloud.com>
2019-10-24 16:16:48 +0800

    telegram api

--- src/main/java/com/sw/note/api/EmailController.java
+++ src/main/java/com/sw/note/api/EmailController.java
@@ -1,20 +1,28 @@
 package com.sw.note.api;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sw.note.util.MailUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import tk.mybatis.mapper.util.StringUtil;
 
 @Api(value = "邮件", description = "邮件", tags = "6")
 @RestController
 @RequestMapping(path = "/email")
 public class EmailController {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${telegram.token}")
+    public String token;
+    @Value("${telegram.chatId}")
+    public String chatId;
+
     @ApiOperation(value = "发送", notes = "发送")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "receiver", paramType = "query", value = "receiver", required = true, dataType = "String"),
@@ -34,5 +42,17 @@ public class EmailController {
         } catch (Exception e) {
             return "0";
         }
+    }
+
+    @ApiOperation(value = "发送", notes = "发送")
+    @GetMapping("/telegram")
+    public boolean send(@RequestBody String msg) {
+        String url = String.format("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s", token, chatId, msg);
+        String resp = restTemplate.getForObject(url, String.class);
+        JSONObject jsonObject = JSONObject.parseObject(resp);
+        if (jsonObject.containsKey("ok")) {
+            return jsonObject.getBooleanValue("ok");
+        }
+        return false;
     }
 }
