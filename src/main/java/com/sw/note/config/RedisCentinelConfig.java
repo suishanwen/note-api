34bf52626f52683520742e8ec4d6d3c00549f873
suishanwen <suishanwen@icloud.com>
2020-06-10 22:59:27 +0800

    Update RedisCentinelConfig.java

--- src/main/java/com/sw/note/config/RedisCentinelConfig.java
+++ src/main/java/com/sw/note/config/RedisCentinelConfig.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
